24c9c547e5714645e028510cb93d42b783355b7f
Mariella Pranjic <mariellap.2005@icloud.com>
2026-04-03 19:01:36 +0200

    fix: components access and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+    
+import { ShoppingList } from './components/shopping-list/shopping-list';
+import { FridgeListComponent } from './components/fridge-list/fridge-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'shopping-list', component: ShoppingList },
+  { path: 'fridge', component: FridgeListComponent }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FridgeListComponent } from './fridge-list/fridge-list';
+//import { FridgeListComponent } from './components/fridge-list/fridge-list';
 
 @Component({
   selector: 'app-root',
-  imports: [FridgeListComponent],
+  //imports: [FridgeListComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
