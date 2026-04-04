77426801e11c35053a5b54c00e000cdcec29da88
Mariella Pranjic <mariellap.2005@icloud.com>
2026-04-04 18:03:17 +0200

    add: fonts, colors, shoppinglist page, navbar basics

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 //import { FridgeListComponent } from './components/fridge-list/fridge-list';
 
+import { Navbar } from './components/navbar/navbar';
+
 @Component({
   selector: 'app-root',
   //imports: [FridgeListComponent],
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/components/shopping-list/shopping-list.ts
+++ src/app/components/shopping-list/shopping-list.ts
@@ -7,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './shopping-list.css',
 })
 export class ShoppingList {
+  
+  items = [
+    { name: 'Banane' },
+    { name: 'Apfel' },
+  ];
 
+  //add item to shopping list
+  addItem(inputElement: HTMLInputElement) {
+    const itemName = inputElement.value.trim();
+    
+    if (itemName) {
+      this.items.push({ name: itemName });
+      inputElement.value = '';
+    }
+  }
+
+  //remove item from shopping list
+  removeItem(item: any) {
+    this.items = this.items.filter(i => i !== item);
+  }
 }
