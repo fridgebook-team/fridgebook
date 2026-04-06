f75037f7dbcd427a89849a098984a9f829cf1b47
Mariella Pranjic <mariellap.2005@icloud.com>
2026-04-07 01:38:44 +0200

    add: favorites page - html, css, ts basics

--- src/app/components/favorites/favorites.ts
+++ src/app/components/favorites/favorites.ts
@@ -1,10 +1,94 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-favorites',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './favorites.html',
   styleUrl: './favorites.css',
 })
-export class Favorites {}
+export class Favorites {
+  recipes: Recipe[] = [
+  {
+    id: 0,
+    name: 'Creamy Tuscan Ravioli',
+    image: 'images/recipes/ravioli.jpg',
+    time: '30 Min',
+    isVegan: true,
+    isFavorite: true,
+    matchPercentage: 75,
+    borderColor: '#4A5D23'
+  },
+  {
+    id: 1,
+    name: 'Creamy Tuscan Ravioli',
+    image: 'images/recipes/ravioli.jpg',
+    time: '30 Min',
+    isVegan: true,
+    isFavorite: true,
+    matchPercentage: 75,
+    borderColor: '#4A5D23'
+  },
+  {
+    id: 2,
+    name: 'Creamy Tuscan Ravioli',
+    image: 'images/recipes/ravioli.jpg',
+    time: '30 Min',
+    isVegan: true,
+    isFavorite: true,
+    matchPercentage: 75,
+    borderColor: '#4A5D23'
+  },
+  {
+    id: 1,
+    name: 'Creamy Tuscan Ravioli',
+    image: 'images/recipes/ravioli.jpg',
+    time: '30 Min',
+    isVegan: true,
+    isFavorite: true,
+    matchPercentage: 75,
+    borderColor: '#4A5D23'
+  },
+  {
+    id: 1,
+    name: 'Creamy Tuscan Ravioli',
+    image: 'images/recipes/ravioli.jpg',
+    time: '30 Min',
+    isVegan: true,
+    isFavorite: true,
+    matchPercentage: 75,
+    borderColor: '#4A5D23'
+  },
+  {
+    id: 1,
+    name: 'Creamy Tuscan Ravioli',
+    image: 'images/recipes/ravioli.jpg',
+    time: '30 Min',
+    isVegan: true,
+    isFavorite: true,
+    matchPercentage: 75,
+    borderColor: '#4A5D23'
+  },
+  {
+    id: 1,
+    name: 'Creamy Tuscan Ravioli',
+    image: 'images/recipes/ravioli.jpg',
+    time: '30 Min',
+    isVegan: true,
+    isFavorite: true,
+    matchPercentage: 75,
+    borderColor: '#4A5D23'
+  }
+];
+}
 
+interface Recipe {
+  id: number;
+  name: string;
+  image: string;
+  time: string;
+  isVegan: boolean;
+  isFavorite: boolean;
+  matchPercentage: number;
+  borderColor: string;
+}
