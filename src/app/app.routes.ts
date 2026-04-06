aaed282c4372c70efb806a89ff1fc019fb460d62
Mariella Pranjic <mariellap.2005@icloud.com>
2026-04-06 21:25:56 +0200

    add: routing for all pages, navbar visuals updating

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,14 @@ import { Routes } from '@angular/router';
     
 import { ShoppingList } from './components/shopping-list/shopping-list';
 import { FridgeListComponent } from './components/fridge-list/fridge-list';
+import { Favorites } from './components/favorites/favorites';
+import { Search } from './components/search/search';
+import { Home } from './components/home/home';
 
 export const routes: Routes = [
   { path: 'shopping-list', component: ShoppingList },
+  { path: 'home', component: Home },
+  { path: 'search', component: Search },
+  { path: 'favorites', component: Favorites },
   { path: 'fridge', component: FridgeListComponent }
 ];
